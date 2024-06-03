865a27da679b556bbd8a2a3d9fbe3834b4e1b2e9
sha-abhishek <asharm5@clemson.edu>
2024-06-03 11:31:33 -0400

    test

--- hw1/cs285/scripts/run_hw1.py
+++ hw1/cs285/scripts/run_hw1.py
@@ -157,7 +157,7 @@ def run_training_loop(params):
           # HINT2: use np.random.permutation to sample random indices
           # HINT3: return corresponding data points from each array (i.e., not different indices from each array)
           # for imitation learning, we only need observations and actions.  
-          ob_batch, ac_batch = TODO
+          ob_batch, ac_batch = 0 #TODO
 
           # use the sampled data to train an agent
           train_log = actor.update(ob_batch, ac_batch)
